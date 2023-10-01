c3c787adfba6e1335ca68be93d8a488bba505bb4
Daniel151505 <daniel150520011@gmail.com>
2023-10-01 13:57:43 -0500

    Validando el Presupuesto

--- src/components/Presupuesto.vue
+++ src/components/Presupuesto.vue
@@ -1,7 +1,17 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const presupuesto = ref(0);
+
+const definirPresupuesto = () => {
+  if (presupuesto.value <= 0) {
+    console.log("Presupuesto no valido");
+  }
+};
+</script>
 
 <template>
-  <form class="presupuesto">
+  <form class="presupuesto" @submit.prevent="definirPresupuesto">
     <div class="campo">
       <label for="nuevo-presupuesto">Definir Presupuesto</label>
       <input
@@ -9,6 +19,8 @@
         class="nuevo-presupuesto"
         placeholder="Añade tu presupuesto"
         type="number"
+        min="0"
+        v-model.number="presupuesto"
       />
     </div>
     <input type="submit" value="Definir Presupuesto" />
