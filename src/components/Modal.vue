04105cdea83850523378369980a4523d871087f9
Daniel151505 <daniel150520011@gmail.com>
2023-10-04 21:12:04 -0500

    Añadiendo el formulario en el modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -9,6 +9,41 @@ const emit = defineEmits(["ocultar-modal"]);
     <div class="cerrar-modal">
       <img :src="cerrarModal" alt="cerrar" @click="$emit('ocultar-modal')" />
     </div>
+
+    <div class="contenedor">
+      <form>
+        <legend>Añadir Gasto</legend>
+        <div class="campo">
+          <label for="nombre">Nombre Gasto:</label>
+          <input
+            type="text"
+            id="nombre"
+            placeholder="Añade el nombre del gasto"
+          />
+        </div>
+        <div class="campo">
+          <label for="cantidad">Cantidad:</label>
+          <input
+            type="number"
+            id="cantidad"
+            placeholder="Añade la cantidad del gasto, ej. 300"
+          />
+        </div>
+        <div class="campo">
+          <label for="categoria">Categoría:</label>
+          <select id="categoria">
+            <option value="">--Seleccione--</option>
+            <option value="ahorro">Ahorro</option>
+            <option value="comida">Comida</option>
+            <option value="casa">Casa</option>
+            <option value="ocio">Ocio</option>
+            <option value="salud">Salud</option>
+            <option value="suscripciones">Suscripciones</option>
+          </select>
+        </div>
+        <input type="submit" value="Añadir gasto" />
+      </form>
+    </div>
   </div>
 </template>
 
