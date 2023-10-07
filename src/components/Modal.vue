328945a0fdbeac43c99d116247f71c4719b5ae2d
Daniel151505 <daniel150520011@gmail.com>
2023-10-06 23:02:13 -0500

    Creando un botón para eliminar gastos

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -149,6 +149,7 @@ const isEditing = computed(() => {
           :value="[isEditing ? 'Guardar Cambios' : 'Añadir Gasto']"
         />
       </form>
+      <button type="button" class="btn-eliminar" v-if="isEditing">Eliminar</button>
     </div>
   </div>
 </template>
@@ -217,6 +218,18 @@ const isEditing = computed(() => {
   background-color: var(--azul);
   color: var(--blanco);
   font-weight: 700;
+  cursor: pointer;
+}
+
+.btn-eliminar {
+  border: none;
+  padding: 1rem;
+  width: 100%;
+  background-color: #ef4444;
+  font-weight: 700;
+  font-size: 1.2rem;
+  color: var(--blanco);
+  margin-top: 10rem;
   cursor: pointer;
 }
 </style>
