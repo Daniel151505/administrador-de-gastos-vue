3df03f458b1654ef7f89987fb05468a354a5a103
Daniel151505 <daniel150520011@gmail.com>
2023-10-06 22:30:48 -0500

    Evitar registros duplicados

--- src/App.vue
+++ src/App.vue
@@ -86,10 +86,18 @@ const ocultarModal = () => {
 };
 
 const guardarGasto = () => {
-  gastos.value.push({
-    ...gasto,
-    id: generarId(),
-  });
+  if (gasto.id) {
+    //editando
+    const { id } = gasto;
+    const i = gastos.value.findIndex((gasto) => gasto.id === id);
+    gastos.value[i] = { ...gasto };
+  } else {
+    // registro nuevo
+    gastos.value.push({
+      ...gasto,
+      id: generarId(),
+    });
+  }
 
   ocultarModal();
 
